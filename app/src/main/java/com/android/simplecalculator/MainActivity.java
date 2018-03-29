29c29a112e4448c22aee4a7a9c120e8a9ec8a468
Andy Or <awor@ucsd.edu>
2018-03-28 21:38:42 -0700

    Fixed crash on start. Basic operations work.

--- app/src/main/java/com/android/simplecalculator/MainActivity.java
+++ app/src/main/java/com/android/simplecalculator/MainActivity.java
@@ -1,14 +1,15 @@
 package com.android.simplecalculator;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Locale;
 import java.util.Stack;
 
 /**
@@ -20,31 +21,31 @@ public class MainActivity extends AppCompatActivity{
     private Stack<String> numStack = new Stack<>(); // input and numbers
     private Stack<String> opStack = new Stack<>(); // postfix and operators
     private Hashtable<String, Integer> orderOfOps =  new Hashtable<>();
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private long result;
 
-    // Buttons on Calculator
-    EditText userInput = (EditText) findViewById(R.id.output);
-    final Button add = (Button) findViewById(R.id.add);
-    final Button subtract = (Button) findViewById(R.id.subtract);
-    final Button multiply = (Button) findViewById(R.id.multiply);
-    final Button divide = (Button) findViewById(R.id.divide);
-    final Button equal = (Button) findViewById(R.id.equal);
-    final Button num0 = (Button) findViewById(R.id.num0);
-    final Button num1 = (Button) findViewById(R.id.num1);
-    final Button num2 = (Button) findViewById(R.id.num2);
-    final Button num3 = (Button) findViewById(R.id.num3);
-    final Button num4 = (Button) findViewById(R.id.num4);
-    final Button num5 = (Button) findViewById(R.id.num5);
-    final Button num6 = (Button) findViewById(R.id.num6);
-    final Button num7 = (Button) findViewById(R.id.num7);
-    final Button num8 = (Button) findViewById(R.id.num8);
-    final Button num9 = (Button) findViewById(R.id.num9);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userInput.setText(" ");
-
+        // Buttons on Calculator
+        final EditText userInput = findViewById(R.id.output);
+        final Button add = findViewById(R.id.add);
+        final Button subtract = findViewById(R.id.subtract);
+        final Button multiply = findViewById(R.id.multiply);
+        final Button divide = findViewById(R.id.divide);
+        final Button equal = findViewById(R.id.equal);
+        final Button num0 = findViewById(R.id.num0);
+        final Button num1 = findViewById(R.id.num1);
+        final Button num2 = findViewById(R.id.num2);
+        final Button num3 = findViewById(R.id.num3);
+        final Button num4 = findViewById(R.id.num4);
+        final Button num5 = findViewById(R.id.num5);
+        final Button num6 = findViewById(R.id.num6);
+        final Button num7 = findViewById(R.id.num7);
+        final Button num8 = findViewById(R.id.num8);
+        final Button num9 = findViewById(R.id.num9);
 
         // PEMDAS
         orderOfOps.put("+", 1);
@@ -58,33 +59,36 @@ public class MainActivity extends AppCompatActivity{
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp(" + ");
+                userInput.setText(append(userInput.getText().toString(), " + "));
             }
         });
         subtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp(" - ");
+                userInput.setText(append(userInput.getText().toString(), " - "));
             }
         });
         multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp(" * ");
+                userInput.setText(append(userInput.getText().toString(), " * "));
             }
         });
         divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp(" / ");
+                userInput.setText(append(userInput.getText().toString()," / "));
             }
         });
         equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toPostFix();    // change infix expression to postfix
-                evaluate();     // evaluate all of stack
-
+                Log.d(TAG, "equal() on infix : " + userInput.getText().toString());
+                toPostFix(userInput.getText().toString());    // change infix expression to postfix
+                Log.d(TAG, "postfix in numStack, ready to evaluate");
+                result = evaluate();     // evaluate all of stack
+                userInput.setText(String.format(Locale.US, "%d",    result));
+                Log.d(TAG, "result = " + result);
             }
         });
 
@@ -93,61 +97,61 @@ public class MainActivity extends AppCompatActivity{
         num0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("0");
+                userInput.setText(append(userInput.getText().toString(),"0"));
             }
         });
         num1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("1");
+                userInput.setText(append(userInput.getText().toString(),"1"));
             }
         });
         num2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("2");
+                userInput.setText(append(userInput.getText().toString(),"2"));
             }
         });
         num3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("3");
+                userInput.setText(append(userInput.getText().toString(),"3"));
             }
         });
         num4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("4");
+                userInput.setText(append(userInput.getText().toString(),"4"));
             }
         });
         num5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("5");
+                userInput.setText(append(userInput.getText().toString(),"5"));
             }
         });
         num6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("6");
+                userInput.setText(append(userInput.getText().toString(),"6"));
             }
         });
         num7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("7");
+                userInput.setText(append(userInput.getText().toString(),"7"));
             }
         });
         num8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("8");
+                userInput.setText(append(userInput.getText().toString(),"8"));
             }
         });
         num9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appendExp("9");
+                userInput.setText(append(userInput.getText().toString(),"9"));
             }
         });
 
@@ -209,16 +213,16 @@ public class MainActivity extends AppCompatActivity{
 
         switch (operator) {
             case "+":
-                result = Integer.parseInt(op1) + Integer.parseInt(op2);
+                result = Integer.parseInt(op2) + Integer.parseInt(op1);
                 break;
             case "-":
-                result = Integer.parseInt(op1) - Integer.parseInt(op2);
+                result = Integer.parseInt(op2) - Integer.parseInt(op1);     // operand order matters
                 break;
             case "/":
                 result = Integer.parseInt(op2) / Integer.parseInt(op1);     // operand order matters
                 break;
             case "*":
-                result = Integer.parseInt(op1) * Integer.parseInt(op2);
+                result = Integer.parseInt(op2) * Integer.parseInt(op1);
                 break;
             default:
                 result = 0;
@@ -242,9 +246,8 @@ public class MainActivity extends AppCompatActivity{
      * Source of Help:
      *  http://interactivepython.org/runestone/static/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
      */
-    public void appendExp(String c) {
-        String added = userInput.getText() + c;
-        userInput.setText(added);
+    public String append(String currentInput, String append) {
+        return currentInput + append;
     }
 
     /*
@@ -252,21 +255,23 @@ public class MainActivity extends AppCompatActivity{
      * Source of Help:
      *  http://interactivepython.org/runestone/static/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
      */
-    public void toPostFix() {
-        String[] expression = userInput.getText().toString().split(" ");
+    public void toPostFix(String infix) {
+        String[] expression = infix.split(" ");
         for (String input: expression ) {
 
             if( input.equals("(")) {
                 opStack.push(input);
 
             } else if ( input.equals(")")) {
-                while (!opStack.peek().equals("("))
+                while (!opStack.peek().equals("(") )
                     numStack.push(opStack.pop());
 
             } else if( isOperator(input) ) {
                 // remove any  precedence operators on opstack
+       //         Log.d(TAG, "input = " + input);
+//                Log.d(TAG, "top of op stack is " + opStack.peek());
                 while (!opStack.isEmpty() &&
-                        orderOfOps.get(input) < orderOfOps.get(opStack.peek())) {
+                        (orderOfOps.get(input) < orderOfOps.get(opStack.peek()))  ) {
                     numStack.push(opStack.pop());
                 }
                 opStack.push(input);
